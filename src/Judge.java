d17795558c27d8e2e43fa016409573bd77749b96
fedegove <federico.governatori07@gmail.com>
2026-01-16 19:14:02 +0100

    l'ho praticamente rifatto da capo che ho fatto prima

--- src/Judge.java
+++ src/Judge.java
@@ -1,5 +1,77 @@
-public class Judge {
-    public void verifica() {
+import java.util.ArrayList;
 
+public class Judge {
+    
+    private String finalPlanet;
+    private int planetDistance;
+    private boolean started = false;
+    
+    private ArrayList<Ship> ships;
+    private ArrayList<Ship> ranking;
+
+    public Judge(String finalPlanet, int planetDistance) {
+        this.finalPlanet = finalPlanet;
+        this.planetDistance = planetDistance;
+        this.ships = new ArrayList<>();
+        this.ranking = new ArrayList<>();
+    }
+
+    public void addShip(Ship ship) {
+        ships.add(ship);
+    }
+
+    public void startRace() {
+        
+        System.out.println("La gara verso: " + finalPlanet + " è iniziata!");
+        System.out.println("Partecipanti: " + ships.size());
+
+        for (Ship ship : ships) {
+            ship.start();
+        }
+
+        try {
+            Thread.sleep(3000);
+
+            started = true;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }    
+
+    public void waitForFinish() {
+        for (Ship ship : ships) {
+            try {
+                ship.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public synchronized void recordFinish(Ship ship) {
+        ranking.add(ship);
+        
+        if (ranking.size() == ships.size()) {
+            showFinalRanking();
+        }
+
+    }
+
+    private void showFinalRanking() {
+        System.out.println("La gara è terminata! Ecco la classifica finale: ");
+        
+        for (int i = 0; i < ranking.size(); i++) {
+            Ship ship = ranking.get(i);
+            System.out.println((i + 1) + "° posto: " + ship.getType() + " con pilota " + ship.getPilot());
+        }
+
+    }
+
+    public int getPlanetDistance() {
+        return planetDistance;
+    }
+
+    public String getFinalPlanet() {
+        return finalPlanet;
     }
 }
--- src/Ship.java
+++ src/Ship.java
@@ -1,19 +1,77 @@
 import java.util.Random;
 
-public class Ship implements Runnable {
-    private String name;
+public class Ship extends Thread{
+    private String type;
+    private String pilot;
+    private String destination;
+    
     private Judge judge;
+    private int shipId;
+    private int distanceTraveled = 0;
     private int speed;
+
+    private boolean inRace = true;
 
     Random random = new Random();
 
-    public Ship(String name, Judge judge) {
-        this.name = name;
+    public Ship(String type, int shipId, String pilot, Judge judge, String destination, int speed) {
+        this.type = type;
+        this.shipId = shipId;
+        this.pilot = pilot;
+        this.judge = judge;
+        this.destination = destination;
         this.speed = random.nextInt(100) + 1;
     }
 
     @Override
     public void run() {
+        System.out.println("Astronave " + type + " pilotata da " + pilot + " pronta per arrivare a " + destination);
 
+        race();
+    }
+
+    private void race() {
+        while (inRace && distanceTraveled < judge.getPlanetDistance()) {
+            try {
+                Thread.sleep(5000);
+
+                distanceTraveled += speed;
+
+                if (distanceTraveled >= judge.getPlanetDistance()) {
+                distanceTraveled = judge.getPlanetDistance();
+                inRace = false;
+                System.out.println("Astronave " + type + " pilotata da " + pilot + " ha raggiunto " + destination + "!");
+                judge.recordFinish(this);
+                } else {
+                System.out.println("Astronave " + type + " pilotata da " + pilot + " mancano " + (judge.getPlanetDistance() - distanceTraveled) + " km verso " + destination);
+                }
+            } catch (InterruptedException e) {
+                inRace = false;
+            }
+        }
+    }
+
+    public int getShipId() {
+        return shipId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getPilot() {
+        return pilot;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getDistanceTraveled() {
+        return distanceTraveled;
     }
 }
